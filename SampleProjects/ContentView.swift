438fe9158f51472aff2b25e197740d0fd79fdb91
Maksym <maksim.leshchemko.dev@gmail.com>
2022-09-11 13:04:01 +0300

    Initial Commit

---
+++ SampleProjects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleProjects
+//
+//  Created by Maksym Leshchenko on 11.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleProjects/SampleProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectsApp.swift
+//  SampleProjects
+//
+//  Created by Maksym Leshchenko on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
